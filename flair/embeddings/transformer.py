db83c20ab0869002b3023c69864b5ced0b06fe69
Valerio Sofi <valeriosofi@MacBook-Pro-di-Valerio.local>
2022-09-08 10:58:40 +0200

    fix

--- flair/embeddings/transformer.py
+++ flair/embeddings/transformer.py
@@ -778,7 +778,7 @@ class TransformerNebullvmEmbeddings(TransformerBaseEmbeddings):
         return {"token_embeddings": self.model(*tensors.values())[0]}
 
     @classmethod
-    def convert_dynamic_axes(cls, dynamic_info: Dict):
+    def convert_dynamic_axes(cls, dynamic_info: Dict[str, Dict[int, str]]) -> Dict[str, List[Dict[int, str]]]:
         dynamic_axes = dict()
         dynamic_axes["inputs"] = []
         dynamic_axes["outputs"] = []
